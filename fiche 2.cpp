2e2a9c2824081a4f4feeedb5683d2c283bb45235
jimeeam <139251508+jimeeam@users.noreply.github.com>
2023-08-08 10:01:31 -0600

    Add files via upload

---
+++ fiche 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+void enviar_a_archivo() {
+    ofstream archivo("jimee.txt", ios::app);
+
+    if (!archivo.is_open()) {
+        cout << "Error al abrir el archivo.\n";
+        return;
+    }
+    string dato;
+    cout << "Ingrese un dato: ";
+    getline(cin, dato);
+
+    archivo << dato << endl;
+
+    archivo.close();
+    cout << "Dato enviado correctamente al archivo.\n";
+}
+int main() {
+    enviar_a_archivo();
+ return 0;
+}
